6e99f1c6d4d0e30f98a179f6c7b1298d672ce522
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2025-02-24 18:06:47 +0100

    Add default login menu

---
+++ LU2-IndividueelProject/Assets/Scenes/Scripts/ScriptGameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ScriptGameManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
