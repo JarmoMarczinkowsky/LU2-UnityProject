8883cb5cecdac27504ec24150882c3c5b9d58c41
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2025-03-06 14:58:00 +0100

    Added connection with database

--- LU2-IndividueelProject/Assets/Scripts/ScriptLoginManager.cs
+++ LU2-IndividueelProject/Assets/Scripts/ScriptLoginManager.cs
@@ -1,11 +1,46 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ScriptLoginManager : MonoBehaviour
 {
-    public void StartGame()
+    [Header("UI Elements")]
+    public TMP_InputField UsernameField;
+    public TMP_InputField PasswordField;
+
+    [Header("Dependencies")]
+    public UserApiClient userApiClient;
+    public Environment2DApiClient enviroment2DApiClient;
+    public Object2DApiClient object2DApiClient;
+
+    
+    public async void StartGame()
     {
-        SceneManager.LoadScene("SceneMenuWorld");
+        User user = new User()
+        {
+            email = UsernameField.text.ToLower().Trim(),
+            password = PasswordField.text
+        };
+
+        IWebRequestReponse webRequestResponse = await userApiClient.Login(user);
+
+        switch (webRequestResponse)
+        {
+            case WebRequestData<string> dataResponse:
+                Debug.Log("Login succes!");
+                // TODO: Todo handle succes scenario.
+                break;
+            case WebRequestError errorResponse:
+                string errorMessage = errorResponse.ErrorMessage;
+                Debug.Log("Login error: " + errorMessage);
+                // TODO: Handle error scenario. Show the errormessage to the user.
+                break;
+            default:
+                throw new NotImplementedException("No implementation for webRequestResponse of class: " + webRequestResponse.GetType());
+        }
+
+        //SceneManager.LoadScene("SceneMenuWorld");
     }
 
     public void QuitGame()
